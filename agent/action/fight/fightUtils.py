2b57b26cf7b0b34584ba17d702828014670d5724
mint <654899405@qq.com>
2025-06-20 23:53:11 +0800

    chore: 优化使用物品的检测流程

--- agent/action/fight/fightUtils.py
+++ agent/action/fight/fightUtils.py
@@ -368,10 +368,12 @@ def findItem(
                 )
                 context.tasker.controller.post_click(center_x, center_y).wait()
                 time.sleep(1)
-                LoadItemdetail = context.run_task("Bag_LoadItem")
+
+                if context.run_recognition("Bag_LoadItem"):
+                    context.run_task("Bag_LoadItem")
                 # 使用所有
-                if not LoadItemdetail:
-                    LoadItemdetail = context.run_task("Bag_LoadAllItem")
+                else:
+                    context.run_task("Bag_LoadAllItem")
 
                 if (dst_x != 0) or (dst_y != 0):
                     logger.info(f"使用物品 {equipmentName}, at {dst_x},{dst_y}")
