6e3e5d9c26ba72ce10be08e18d16113d9802e62f
yyc2644 <yichengace@hotmail.com>
2026-06-30 10:23:41 +0800

    perf: 增加mars出图楼层159和mars149学习巨龙称号逻辑 (#179)
    * 1.繁星挂证后,吃满奖励为60/4 = 150层,增加了出图选项
    2.修改了巨龙称号,不出图也先点到4级
    
    * 限制Mars 86层巨龙称号学习条件

--- agent/action/mars/mars_title.py
+++ agent/action/mars/mars_title.py
@@ -89,10 +89,14 @@ class MarsTitleManager:
                 fightUtils.title_learn_branch("恶魔", 5, "生命强化", 3, context)
             else:
                 logger.info("没点恶魔")
-            if fightUtils.title_check("巨龙", context):
+            if self.mars.target_leave_layer_para >= 149 and fightUtils.title_check(
+                "巨龙", context
+            ):
                 self.isGetDragonTitle = True
                 fightUtils.title_learn("巨龙", 1, "亚龙血统", 3, context)
                 fightUtils.title_learn("巨龙", 2, "初级龙族血统", 3, context)
+                fightUtils.title_learn("巨龙", 3, "中级龙族血统", 3, context)
+                fightUtils.title_learn("巨龙", 4, "高级龙族血统", 3, context)
 
             context.run_task("Fight_ReturnMainWindow")
 
